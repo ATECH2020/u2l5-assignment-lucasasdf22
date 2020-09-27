248d632fe8b4f04594d8abb2fc1b874f6853f0de
Lucas Fernandez <lucas.236598@nv.ccsd.net>
2020-09-27 19:57:18 +0000

    Completed Construction Costs and How Far Away Is...?

--- src/main/java/ConstructionTester.java
+++ src/main/java/ConstructionTester.java
@@ -3,7 +3,31 @@ import java.util.Scanner;
 public class ConstructionTester
 {
     public static void main(String[] args)
-    {
-        // Start here!
+    {   
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Enter the sales tax rate");
+        double taxRate = in.nextDouble();
+
+        double lumberCost = 8.0;
+        double windowCost = 11.0;
+
+        Construction construct = new Construction(lumberCost, windowCost, taxRate);
+
+        System.out.println("How many boards do you need?");
+        int numBoards = in.nextInt();
+
+        System.out.println("How many windows do you need?");
+        int numWindows = in.nextInt();
+
+        double total;
+        total = construct.lumberCost(numBoards) + construct.windowCost(numWindows);
+
+        double grandTotal;
+
+        grandTotal = construct.grandTotal(total);
+
+        System.out.println("Total: " + total);
+        System.out.println("Grand Total: " + grandTotal);
     }
 }
--- src/main/java/HowFarAway.java
+++ src/main/java/HowFarAway.java
@@ -4,6 +4,25 @@ public class HowFarAway
 {
     public static void main(String[] args)
     {
-        // your code here.
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Enter the latitude of the starting location: ");
+        double sLatitude = in.nextDouble();
+
+        System.out.print("Enter the longitude of the starting location: ");
+        double sLongitude = in.nextDouble();
+
+        System.out.print("Enter the latitude of the ending location: ");
+        double eLatitude = in.nextDouble();
+
+        System.out.print("Enter the longitude of the ending location: ");
+        double eLongitude = in.nextDouble();
+
+        GeoLocation geoL = new GeoLocation(sLatitude, sLongitude);
+        GeoLocation geoL2 = new GeoLocation(eLatitude, eLongitude);
+
+        double distance = geoL.distanceFrom(geoL2);
+
+        System.out.println("The distance is " + distance + " miles.");
     }
 }
